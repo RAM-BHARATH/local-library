5dd51b773e976777f820964f22f9b93987c4bf37
RAM-BHARATH <ramtherapid007@gmail.com>
2022-07-22 17:32:01 +0530

    Updated env

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = process.env.MONGODB_URI;
+var mongoDB = 'mongodb+srv://mdnuser1:3Kz7i6i71Li6EAwT@cluster0.gbunfej.mongodb.net/local_library?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
